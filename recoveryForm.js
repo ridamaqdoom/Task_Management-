e2a0884154cde59074dd974c1d6e91b02b0ed251
Quinn <qpb948@usask.ca>
2023-11-30 23:08:41 -0600

    Added some documentation to recovery form files. Added remove recovery form.

--- recoveryForm.js
+++ recoveryForm.js
@@ -16,18 +16,17 @@ const recoverySchema = new mongoose.Schema({
 const recoveryForm = mongoose.model("Recovery", recoverySchema);
 
 function setup(app) {
-  // Get the animals that are currently logged in to populate the dropdown in createRecoveryForm
+  // Retrieve a client's animals
   app.get("/getAnimals", auth.authenticateToken, async (req, res) => {
     try {
-      // Assuming findClient is a function that finds a client based on the username
-      const client = await findClient(req.query.username); // Use req.query.username to get the username from the query parameter
+      // Find the client using the username
+      const client = await findClient(req.query.username);
       if (!client) {
+        // Return an error if the client is not found
         return res.status(404).json({ error: "Client not found" });
       }
-
-      // Assuming client.animals is an array of animal data
+      // Retrieve and send the client's animals
       const animals = client.animals;
-      // Send the list of animals as a response
       res.status(200).json({ animals });
     } catch (error) {
       console.error(error.message);
@@ -35,7 +34,9 @@ function setup(app) {
     }
   });
 
+  // Save a new recovery form
   app.post("/saveRecoveryForm", auth.authenticateToken, (req, res) => {
+    // Create a new recovery form based on the incoming request data
     const newForm = new recoveryForm({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -46,6 +47,8 @@ function setup(app) {
       issue: req.body.issue,
       comments: req.body.comments,
     });
+
+    // Save the new form to the database
     newForm
       .save()
       .then((form) => {
@@ -57,6 +60,43 @@ function setup(app) {
         res.status(500).json({ error: "Error Creating Form" });
       });
   });
+
+  app.get(
+    "/getAnimalRecoveryForm",
+    auth.authenticateToken,
+    async (req, res) => {
+      const animalID = req.query.animalID;
+
+      try {
+        const formInformation = await recoveryForm.find({
+          animalID: animalID,
+        });
+        res.status(200).json(formInformation);
+      } catch {
+        res.status(500).json({ error: "Error getting form information" });
+      }
+    }
+  );
+
+  app.delete(
+    "/deleteRecoveryForm/:formId",
+    auth.authenticateToken,
+    async (req, res) => {
+      const formId = req.params.formId;
+
+      try {
+        const deletedForm = await recoveryForm.findByIdAndDelete(formId);
+        if (!deletedForm) {
+          return res.status(404).json({ error: "Form not found" });
+        }
+
+        res.status(200).json({ message: "Form deleted successfully" });
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
+  );
 }
 
 module.exports = {
